8d194fefd38c026d7b2e2d81cf5d8778307ce5c5
Isaiah Santala <bigboyswagcakes@Isaiahs-MBP.frontierlocal.net>
2019-05-23 17:38:49 -0700

    Added music to the game to make it more enjoyable, however I am having trouble getting all of it to load into the browser

--- app/app.js
+++ app/app.js
@@ -42,16 +42,26 @@
         [X] - combat messages should be in pop ups
         [X] - searching for loot should be if pop ups
         [X] - item uses should be in pop ups
+
+GAME IS NOT VERY FUN...
+    What could make it more fun?
+   - I think the most doable thing to make the game more fun would be to add sound effects and music
+   [ ] - all buttons should have sounds
+   [ ] - item uses should have sounds
+   [ ] - should have theme music in prep phase
+   [ ] - should have boss music with bosses 
+   [ ] - player att should have sound, crit miss and hit sohuld have special sound
+   [ ] - enemy attack should have sound
+   [ ] - add game over music
+   [ ] - 
+
+
   IF TIME LEFT AFTER:
-    [ ] - make use item pop up buttons nicer
+    [X] - make use item pop up buttons nicer
     [ ] - make item description menu better, 
         [ ] - display quantity of item
-        [ ] - adjust rarity of items
-    [ ] - add critcal hit and critical miss messages to combat
-
-
-
-
+        [X] - adjust rarity of items
+    [X] - add critcal hit and critical miss messages to combat
     [ ] - write a hint menu that randomly generates hints at game over screen
     [ ] - allow for actual game restart without needing to refresh browser
     [ ] - translate ADVANCED list into check boxes
@@ -68,14 +78,22 @@
       5. add in randomized memos for each action button
       6. add animtations
 
+
   */
 
   //POTENTIAL ITEMS:
+
   var potentialLootObj = {
-    "potion":{"qt":1,"desc":"Heals some hp"},
+    "potion":{"qt":1,"desc":"Your health has been restored"},
+    "potion":{"qt":1,"desc":"Your health has been restored"},
+    "potion":{"qt":1,"desc":"Your health has been restored"},
     "wand":{"qt":1,"desc":"Deals 5 magic damage to enemy"},
-    "book":{"qt":1,"desc":"You feel a bit smarter already"},
+    "wand":{"qt":1,"desc":"Deals 5 magic damage to enemy"},
+    "wand":{"qt":1,"desc":"Deals 5 magic damage to enemy"},
+    "book":{"qt":1,"desc":"You feel a bit smarter already \+3AP"},
+    "book":{"qt":1,"desc":"You feel a bit smarter already \+3AP"},
     "lint":{"qt":1,"desc":"Not very useful"},
+    "boots":{"qt":1,"desc":"You put on the boots and feel light as a feather."},
     "boots":{"qt":1,"desc":"You put on the boots and feel light as a feather."},
     "fairy":{"qt":1,"desc":"A mysterious little creature"}
   }
@@ -147,6 +165,7 @@
     else if (type === "messages") {
       $(".messages").css("display", "block")
       $("#ok").click(function() {
+        $("#okSFX")[0].play()
         closeMenu("messages")
       })
     }
@@ -240,7 +259,6 @@
           openMenu("messages")
       }
       $(".items").click(function() {
-          event.preventdefault()
           itemFunc($(this).text())
       })
     }
@@ -267,6 +285,8 @@
 
 
     var resetToPrepPhase = function() {
+      $("#fightMusic")[0].pause()
+      $("#themeMusic")[0].play()
       $(".vs").css("visibility", "hidden")
       $("#enemyBox").css("visibility", "hidden")
       $("#attackButton").css("visibility", "hidden")
@@ -292,19 +312,21 @@
     $("#lootButton").click(function() {
       // CHECK AP
       if (playerData["ap"] > 0) {
-      var currentLoot = getLootButton(potentialLootObj)
-      playerData["ap"] = playerData["ap"] - 1
-      // CHECK IF PLAYER ALREADY HAS ITEM
-      var currentPlayerItems = Object.keys(playerDataLoot)
-      if (currentPlayerItems.includes(currentLoot)) {
-        playerDataLoot[currentLoot]["qt"] = playerDataLoot[currentLoot]["qt"] + 1
-      }
-      else {
-        playerDataLoot[currentLoot] = potentialLootObj[currentLoot]
-      }
-      getLoot("#playerLootDisplay", "items")
-      getAp()
+        $("#searchSFX")[0].play()
+        var currentLoot = getLootButton(potentialLootObj)
+        playerData["ap"] = playerData["ap"] - 1
+        // CHECK IF PLAYER ALREADY HAS ITEM
+        var currentPlayerItems = Object.keys(playerDataLoot)
+        if (currentPlayerItems.includes(currentLoot)) {
+          playerDataLoot[currentLoot]["qt"] = playerDataLoot[currentLoot]["qt"] + 1
+        }
+        else {
+          playerDataLoot[currentLoot] = potentialLootObj[currentLoot]
+        }
+        getLoot("#playerLootDisplay", "items")
+        getAp()
       } else {
+        $("#cannotDoSFX")[0].play()
         $("#currentMessage").text("You do not have enough Action Points to search for loot right now \n \n \n*hint* Win fights to earn more AP")
         openMenu("messages")
       }
@@ -313,6 +335,7 @@
     $("#trainButton").click(function() {
       if (playerData["ap"] >= 3) {
         if (playerData["lvl"] < 3){
+          $("#lvlUpSFX")[0].play()
           $("#currentMessage").text("You have advanced a combat level! \n \n \nYour max hit is now higher")
           openMenu("messages")
           playerData["lvl"] = playerData["lvl"] + 1
@@ -324,16 +347,20 @@
           getLvl()
           getAp()
         } else {
+          $("#cannotDoSFX")[0].play()
           $("#currentMessage").text("You cannot train any higher, \n \n \nYou have reached the MAX COMBAT LVL")
           openMenu("messages")
         }
       } else {
+        $("#cannotDoSFX")[0].play()
         $("#currentMessage").text("You do not have enough Action Points to train right now \n \n \n*hint* Win fights to earn more AP")
         openMenu("messages")
       }
     });
   // FIGHT BUTTON
     $("#fightButton").click(function(){
+      $("#themeMusic")[0].pause()
+      $("#fightMusic")[0].play()
       $("#currentMessage").text("CHAMPION, \n \n \nFace your challenger")
       openMenu("messages")
    //ENEMY GENERATOR FUNCTION
@@ -358,11 +385,14 @@
         var playerDmgCap = (Number(playerData["lvl"]) + 1) * 2
         dmg = rollDice(playerDmgCap)
         if (dmg === 0) {
+        $("#missSFX")[0].play()
         $("#currentCMessage").text("Critical miss: You dealt " + dmg +" damage")
         }
         else if (dmg === playerDmgCap) {
+        $("#critSFX")[0].play()
         $("#currentCMessage").text("Critical hit!: You dealt " + dmg +" damage")  
         } else {
+        $("#attSFX")[0].play()
         $("#currentCMessage").text("You dealt " + dmg +" damage")
         }
         openMenu("cMessage")
@@ -374,9 +404,11 @@
     //USE ITEM BUTTON
     $("#useItemButton").click(function(){
       if (Object.keys(playerData["loot"]).length < 1) { //check if theres loot in player inven
+        $("#cannotDoSFX")[0].play()
         $("#currentMessage").text("You don\'t have any items")
         openMenu("messages")
       } else {
+        $("#searchSFX")[0].play()
         getLoot("#storeItems", "battleItems") //fill pop up menu with loot from inven
         openMenu("useItem")                   //open item select div
         // var battleItemAlert = function(itemType) { //item description function
@@ -413,8 +445,8 @@
               console.log(item)
               if (item === "potion"){
                 playerData["hp"] = 10
+                $("#potionSFX")[0].play()
                 getHp()
-                initiateUseItemAlerts(clickedItem)      //consumes the item
                 if (keyExists("currentEnemy")) {          //if enemy exists, use item as cMessage
                   $("#currentCMessage").text(playerDataLoot[item]["desc"])
                   openMenu("cMessage")
@@ -422,34 +454,40 @@
                   $("#currentMessage").text(playerDataLoot[item]["desc"])
                   openMenu("messages")
                 }
+                initiateUseItemAlerts(clickedItem)      //consumes the item
               }
               else if (item === "boots") {
                 if (keyExists("currentEnemy")){
+                  $("#runSFX")[0].play()
                   initiateUseItemAlerts(clickedItem)
                   $("#currentMessage").text("You escape unscathed")
                   openMenu("messages")
                   deleteItem("currentEnemy")
                   resetToPrepPhase()
                 } else {
+                  $("#okSFX")[0].play()
                   $("#currentMessage").text("You could run fast in these!")
                   openMenu("messages")
                 }
               }
               else if (item === "wand") {
                 if (keyExists("currentEnemy")){
+                $("#wandSFX")[0].play() 
                 enemyData["hp"] = enemyData["hp"] - 5
                 getEnemyHp()
-                initiateUseItemAlerts(clickedItem)
                 $("#currentCMessage").text(playerDataLoot[item]["desc"])
                 openMenu("cMessage")
+                initiateUseItemAlerts(clickedItem)
                 // continueCombat()
                 } else {
+                  $("#cannotDoSFX")[0].play()
                   $("#currentMessage").text("Save this item for combat")
                   openMenu("messages")
                 }
               }
               else if (item === "fairy") {
                 if (keyExists("currentEnemy")){
+                  $("#fairySFX")[0].play() 
                   initiateUseItemAlerts(clickedItem)
                   enemyData["hp"] = 0
                   getEnemyHp()
@@ -457,6 +495,7 @@
                   openMenu("cMessage")
                   // continueCombat()
                 } else {
+                  $("#okSFX")[0].play() 
                   $("#currentMessage").text("A mysterious little creature")
                   openMenu("messages")
                 }             
@@ -466,15 +505,18 @@
                 getAp()
                 if (keyExists("currentEnemy")){
                   // continueCombat()
+                  $("#cannotDoComSFX")[0].play()
                   $("#currentMessage").text("This is no time for reading!")
                   openMenu("messages")
                 } else {
+                  $("#bookSFX")[0].play()
                   $("#currentMessage").text(playerDataLoot[item]["desc"])
                   openMenu("messages")
                   initiateUseItemAlerts(clickedItem)
                 }             
               }
               else if (item === "lint") {
+              $("#okSFX")[0].play()
               initiateUseItemAlerts(clickedItem)  
               $("#currentMessage").text("not very useful...")   
               openMenu("messages")                
@@ -487,11 +529,13 @@
     $("#runButton").click(function(){
         var runRollDice = rollDice(2)
         if (runRollDice === 0)  {
+          $("#runSFX")[0].play()
           $("#currentMessage").text("You escape unscathed")
           openMenu("messages")
           deleteItem("currentEnemy")
           resetToPrepPhase()
         } else {
+          $("#cannotDoComSFX")[0].play()
           $("#currentCMessage").text("You turn to flee, \n \n \nbut your enemy is too fast")
           openMenu("cMessage")
           // enemyAttack()
@@ -503,6 +547,7 @@
     $(".cMessage").click(function() {
       if (enemyData["hp"] < 1) {
         closeMenu("cMessage")
+        $("#vicSFX")[0].play()
         $("#currentMessage").text("You have slain an enemy!")
         openMenu("messages")
         playerData["triumphs"] = Number(playerData["triumphs"]) + 1
@@ -524,9 +569,9 @@
     })
 
   var enemyAttack = function() {
+      $("#attSFX")[0].play()
       var dmgCap = (Number(enemyData["lvl"]) + 1) * 2
       eDmg = rollDice(dmgCap)
-      console.log(`damage = ${eDmg}`)
       $("#currentEnemyMessage").text(enemyData["name"] + " dealt " + eDmg +" damage")
       openMenu("enemyMessage")
       playerData["hp"] = playerData["hp"] - eDmg
@@ -544,15 +589,16 @@
   $(document).ready(function() {
     // OPEN PLAYER SELECT ON START:
     openMenu("playerSelect")
-    $('#submitPlayer').click(function(event) {
+    $("#submitPlayer").click(function(event) {
       event.preventDefault();
       var playerName = $("#playerName").val();
       if (playerName.length < 1) {
-        $("#currentMessage").text("please name your character")
-        openMenu("messages")
+        alert("please name your character")
       }
       else {
       // createItem("Player", {currentValue:{"hp":10,"triumphs":0,"lvl":1,"loot":{},"ap":1}})
+          $("#startSFX")[0].play()
+          $("#themeMusic")[0].play()
           var playerObj = {"hp":10,"lvl": 1,"loot":{},"triumphs":0, "ap":0}
           createItem(playerName, JSON.stringify(playerObj))
           createItem("potentialLoot", JSON.stringify(potentialLootObj))
